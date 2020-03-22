140a932678069b6e20df7463f12adb6816b9ec6e
Kusumkoli <kusumkoli372@gmail.com>
2020-03-22 01:50:06 -1200

    modal dialog box

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <div id="app">
-    <Header/>
     <Listmenu />
-    <ul>
+    <div class="sticky">
+      Scrolled to bottom: {{ scrolledToBottom }}
+      
+    </div>
+    <button id="myBtn">
+      <p>List 1</p>
+      <p>{{text_out | subStr }} ....</p>
+    </button>
+    <button id="myBtn">
+      <p>List 2</p>
+      <p>{{text_out | subStr }} ....</p>
+    </button>
+    <button id="myBtn">
+      <p>List 3</p>
+      <p>{{text_out | subStr }} ....</p>
+    </button>
+    <button id="myBtn">
+      <p>List 3</p>
+      <p>{{text_out | subStr }} ....</p>
+    </button>
+    <div id="myModal" class="modal">
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <ul>
       <li class="items">type: {{ type }}</li>
       <li class="items">format: {{ format }}</li>
       <li class="items">number: {{ number }}</li>
@@ -12,17 +34,18 @@
       <li class="items" v-html="text_out"></li>
     </ul>
     <img src="https://picsum.photos/350">
-  </div>
+      </div>
+    </div>
+  </div> 
 </template>
 
 <script>
-import Header from './components/Header.vue'
 import Listmenu from './components/Listmenu.vue'
+import main from './main.js'
 
 export default {
   name: 'app',
   components: {
-    Header,
     Listmenu
   },
   data() {
@@ -33,8 +56,23 @@ export default {
       number_max: 100,
       amount: 10,
       time: '',
-      text_out: ''
-    }  
+      text_out: '',
+      scrolledToBottom: false
+    }
+  },
+  function() {
+    return main
+  },
+  methods : {
+    scroll () {
+      window.onscroll = () => {
+        let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
+
+        if (bottomOfWindow) {
+          this.scrolledToBottom = true // replace it with your code
+        }
+      }
+    }
   },
   mounted: function() {
     fetch('http://www.randomtext.me/api')
@@ -50,9 +88,10 @@ export default {
         this.$data.amount = data.amount
         this.$data.time = data.time
         this.$data.text_out = data.text_out
-      })
+      }),
+      this.scroll()
   }
-}
+};
 </script>
 
 <style>
@@ -60,11 +99,55 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
 
-.items {
-  padding: 10px;
+#myBtn {
+  display: block;
+  margin: 100px 0px;
 }
+
+.modal {
+  display: none;
+  position: fixed; 
+  z-index: 1; 
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4); 
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; 
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover, .close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.sticky {
+  position: sticky;
+  top: 0;
+}
+
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('subStr', function(string) {
+    return string.substring(3,20);
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span[0].onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
