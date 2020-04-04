46f48a2e39042500bb25005066fdd11b3dffd2d8
Kusumkoli <kusumkoli372@gmail.com>
2020-04-04 01:07:22 -1200

    refactoring code

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,19 @@
       Scrolled to bottom: {{ scrolledToBottom }} 
     </div>
     <div class="list_items">
-      <div class="myBtn" v-on:click="openModal(); addData0();">
-        <p v-bind:title="queue"><b>Time:</b> {{queue[0].time}}</p>
-        <p v-bind:title="queue">{{queue[0].text_out | subStr}} ....</p>
-      </div>
-      <div class="myBtn" v-on:click="openModal(); addData1();">
-        <p v-bind:title="queue"><b>Time:</b> {{queue[1].time}}</p>
-        <p v-bind:title="queue">{{queue[1].text_out | subStr}} ....</p>
-      </div>
-      <div class="myBtn" v-on:click="openModal(); addData2();">
-        <p v-bind:title="queue"><b>Time:</b> {{queue[2].time}}</p>
-        <p v-bind:title="queue">{{queue[2].text_out | subStr}} ....</p>
-      </div>
-      <div class="myBtn" v-on:click="openModal(); addData3();">
-        <p v-bind:title="queue"><b>Time:</b> {{queue[3].time}}</p>
-        <p v-bind:title="queue">{{queue[3].text_out | subStr}} ....</p>
-      </div>
-      <div class="myBtn" v-on:click="openModal(); addData4();">
-        <p v-bind:title="queue"><b>Time:</b> {{queue[3].time}}</p>
-        <p v-bind:title="queue">{{queue[4].text_out | subStr}} ....</p>
+      <div class="myBtn" v-for="(listItem,index) in queue" v-bind:key="listItem.id" v-on:click="openModal(); addData(index);">
+        <p v-bind:title="listItem"><b>Time:</b> {{listItem.time}}</p>
+        <p v-bind:title="listItem">{{listItem.text_out | subStr}} ....</p>
       </div>
     </div>
     <div id="myModal" class="modal">
       <div class="modal-content">
         <span class="close">&times;</span>
-        <div class="api_data" v-bind:title = "apidata">
-          <p class="items"><b>type:</b> {{ apidata.type }}</p>
-          <p class="items"><b>format:</b> {{ apidata.format }}</p>
-          <p class="items"><b>number:</b> {{ apidata.number }}</p>
-          <p class="items"><b>number_max:</b> {{ apidata.number_max }}</p>
-          <p class="items"><b>amount:</b> {{ apidata.amount }}</p>
-          <p class="items"><b>time:</b> {{ apidata.time }}</p>
-          <p class="items" v-html="apidata.text_out"></p>
+        <div class="api_data">
+          <div v-bind:key = "name.id" v-for="(name, value) in apidata">
+            <p v-if="value=='text_out'" v-html="name"></p>
+            <p v-else class="items"><b>{{value}}:</b> {{ name }}</p>
+          </div>
         </div>
         <div class="picsum_img">
           <img src="https://picsum.photos/350">
@@ -47,9 +28,7 @@
 </template>
 
 <script>
-
 import main from './main.js'
-
 export default {
   name: 'app',
   components: {
@@ -80,7 +59,6 @@ export default {
       span[0].onclick = function() {
         modal.style.display = "none";
       }
-
       window.onclick = function(event) {
         if (event.target == modal) {
           modal.style.display = "none";
@@ -90,7 +68,6 @@ export default {
     scroll: function() {
       window.onscroll = () => {
         let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
-
         if (bottomOfWindow) {
           this.scrolledToBottom = true //replace it with your code
         }
@@ -107,21 +84,8 @@ export default {
       }),
       this.scroll()
     },
-
-    addData0: function() {
-      this.$data.apidata = this.queue[0]
-      console.log(this.apidata)
-    },
-    addData1: function() {
-      this.$data.apidata = this.queue[1]
-      console.log(this.apidata)
-    },
-    addData2: function() {
-      this.$data.apidata = this.queue[2]
-      console.log(this.apidata)
-    },
-    addData3: function() {
-      this.$data.apidata = this.queue[3]
+    addData: function(index) {
+      this.$data.apidata = this.queue[index]
       console.log(this.apidata)
     }
   },
@@ -145,14 +109,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 .sticky {
   position: fixed;
   right: 0%;
   top: 0%;
   padding: 20px;
 }
-
 .modal {
   display: none; /* Hidden by default */
   position: fixed; /* Stay in place */
@@ -167,11 +129,10 @@ export default {
   text-align: left;
   color: #17252a;
 }
-
 /* Modal Content/Box */
 .modal-content {
   background-color: #feffff;
-  margin: 15% auto; /* 15% from the top and centered */
+  margin: 5% auto; /* 5% from the top and centered */
   padding: 20px;
   border: 1px solid #888;
   width: 80%; /* Could be more or less, depending on screen size */
@@ -179,41 +140,33 @@ export default {
   flex-direction: row;
   position: relative;
 }
-
 .myBtn p {
   cursor: pointer;
 }
-
 .list_items {
   margin: 50px 100px;
 }
-
 .myBtn {
   padding: 20px;
   cursor: pointer;
   margin: 10px;
   text-align: left;
 }
-
 .myBtn:nth-child(even) {
   background-color: #def2f1;
 } 
-
 .myBtn:nth-child(odd) {
   background-color: #3aafa9;
 } 
-
 .api_data {
-  padding: 20px;
+  padding: 20px 20px 20px 0px;
   font-size: 15px; 
 }
-
 .picsum_img {
   display: grid;
   align-content: center;
   justify-content: center;
 }
-
 /* The Close Button */
 .close {
   color: #aaa;
@@ -224,28 +177,21 @@ export default {
   font-weight: bold;
   padding: 5px 10px;
 }
-
 h1 {
   color: #2b7a78;
 }
-
 .close:hover,
 .close:focus {
   color: black;
   text-decoration: none;
   cursor: pointer;
 }
-
 @media screen and (max-width: 800px) {
   .modal-content {
     flex-direction: column;
   }
-
   .list_items {
     margin: 10px;
   }
 }
-
 </style>
-
-
